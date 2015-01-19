13ea6fc85309c061aec23b043d7764f14a655d56
bollen <bollen@live.cn>
2015-01-19 18:30:56 +0800

    build mutiple module

---
+++ erp-entities/src/main/java/com/cloud/erp/entities/Module.java
@@ -0,0 +1,42 @@
+package com.cloud.erp.entities;
+
+public class Module
+{
+	private Integer mid;
+	
+	private String title;
+	
+	private String remarks;
+
+	public Integer getMid()
+	{
+		return mid;
+	}
+
+	public void setMid(Integer mid)
+	{
+		this.mid = mid;
+	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+	public String getRemarks()
+	{
+		return remarks;
+	}
+
+	public void setRemarks(String remarks)
+	{
+		this.remarks = remarks;
+	}
+	
+	
+}
---
+++ erp-entities/src/main/java/com/cloud/erp/entities/Role.java
@@ -0,0 +1,42 @@
+package com.cloud.erp.entities;
+
+public class Role
+{
+	private Integer rid;
+	
+	private String roleName;
+	
+	private String remarks;
+
+	public Integer getRid()
+	{
+		return rid;
+	}
+
+	public void setR_id(Integer rid)
+	{
+		this.rid = rid;
+	}
+
+	public String getRoleName()
+	{
+		return roleName;
+	}
+
+	public void setRoleName(String roleName)
+	{
+		this.roleName = roleName;
+	}
+
+	public String getRemarks()
+	{
+		return remarks;
+	}
+
+	public void setRemarks(String remarks)
+	{
+		this.remarks = remarks;
+	}
+	
+	
+}
